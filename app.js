a3697e54e2e23df8ea47ca29705a6c11ea68c2ce
nathern-oss <78829803+nathern-oss@users.noreply.github.com>
2021-12-10 23:48:59 -0500

    Initial Changes

---
+++ app.js
@@ -0,0 +1,16 @@
+var toggle=false;
+
+document.body.querySelector(".menuIcon").addEventListener("click", function (){
+
+    toggle=!toggle;
+
+    if (toggle){
+        document.body.querySelector(".menu").style.right="0";
+    }else{
+        document.body.querySelector(".menu").style.right="-150px";
+    }
+
+
+
+
+})
